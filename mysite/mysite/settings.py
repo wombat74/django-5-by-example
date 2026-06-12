355547377badd7b18438ca7d50cede48735789e3
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2026-06-11 20:15:31 -0700

    added sitemap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,7 +19,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig',
